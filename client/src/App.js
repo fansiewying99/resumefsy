5ca97f7bfbc336e24675eb371ea3bf17d12e8b4a
fansiewying <fsying1999@gmail.com>
2022-03-28 23:48:38 +0800

    commit resume

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,295 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { resume } from "./contents/resume.js"
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  data = null;
+
+  constructor(props) {
+    super(props);
+    this.data = resume();
+    console.log(this.data)
+  }
+
+  componentDidMount() {
+    /*console.log(this.state);
+    resume().then(data => {
+      this.setState({ resume: data })
+      console.log(data);
+    });*/
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container mt-3 mb-3">
+          <div className="hstack gap-3 col-sm-7 mx-auto mb-3">
+            <img src={"/images/" + this.data.profile_img} className="rounded-circle border"
+              style={{ width: '130px', height: 'auto', objectFit: 'cover', objectPosition: 'centerwidth' }} />
+            <div className="container">
+              <div className="ms-4">
+                <h1 className="mb-n1">{this.data.name}</h1>
+                <p style={{ fontSize: '18px' }}>{this.data.subtitle}</p>
+              </div>
+              <div className="mt-n3 hstack gap-3">
+                <div className="col">
+                  <div className="hstack gap-3">
+                    <img src="/images/ic_location.jpg" className="mt-n3 rounded-circle"
+                      style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                    <p style={{ fontSize: '14px' }}>{this.data.location}</p>
+                  </div>
+                  <div className="mt-n3 hstack gap-3 mb-n2">
+                    <img src="/images/ic_contact.jpg" className="mt-n3 rounded-circle"
+                      style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                    <p style={{ fontSize: '14px' }}>{this.data.phone}</p>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="hstack gap-3">
+                    <img src="/images/ic_email.jpg" className="mt-n3 rounded-circle"
+                      style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                    <p style={{ fontSize: '14px' }}>{this.data.email}</p>
+                  </div>
+                  <a href="https://www.linkedin.com/in/fsying99" className="mt-n3 hstack gap-3 mb-n2">
+                    <img src="/images/ic_linkedin.jpg" className="mt-n3 rounded-circle"
+                      style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                    <p style={{ fontSize: '14px' }}>{this.data.linkedin}</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row ms-0 me-0">
+            <div className="col-sm-8">
+              <div className="card border-primary mb-3">
+                <div className="card-header">
+                  <p style={{ fontSize: '20px' }} className="ms-2 mb-n1">Education</p>
+                </div>
+                <div className="card-body">
+                  {this.data.educations.map(edu => (
+                    <div>
+                      <div className="row">
+                        <img src={"/images/" + edu.img} className="mx-auto col-sm-2"
+                          style={{ width: 'auto', height: '50px', objectFit: 'cover' }} />
+                        <div className="col-sm-10">
+                          <h5 className="card-title mb-0">{edu.certificate}</h5>
+                          <div className="hstack">
+                            <img src="/images/ic_location.jpg" className=" rounded-circle"
+                              style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                            <p className="card-text ">{edu.location}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-2 ms-2">
+                        <div className="row">
+                          <p className="card-text col-3 mb-0">Graduation Date</p>
+                          <p className="card-text col-9 mb-0">: {edu.graduation_date}</p>
+                        </div>
+                        <div className="row">
+                          <p className="card-text col-3 mb-0">Result</p>
+                          <p className="card-text col-9 mb-0">: {edu.result}</p>
+                        </div>
+                        <div className="row">
+                          <p className="card-text col-3">Key Modules</p>
+                          <p className="card-text col-9">: {edu.key_modules}</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+
+                </div>
+              </div>
+
+              <div className="card border-primary mb-3">
+                <div className="card-header">
+                  <p style={{ fontSize: '20px' }} className="ms-2 mb-n1">Work Experience</p>
+                </div>
+                <div className="card-body">
+                  {this.data.works.map(work => (
+                    <div>
+                      <div className="row">
+                        <img src={"/images/" + work.img} className="mx-auto col-sm-2"
+                          style={{ width: 'auto', height: '45px', objectFit: 'cover' }} />
+                        <div className="col-sm-10">
+                          <h5 className="card-title mb-0">{work.position}</h5>
+                          <div className="hstack">
+                            <img src="/images/ic_location.jpg" className=" rounded-circle"
+                              style={{ width: '20px', height: 'auto', objectFit: 'cover' }} />
+                            <p className="card-text">{work.location}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-2 ms-2 ">
+                        <ul className="mb-0">
+                          {work.responsibilities.map(desc => (
+                            <li>
+                              {(typeof desc === 'string') ?
+                                desc
+                                :
+                                <ul className="mb-0">
+                                  {desc.map(pt => (
+                                    <li>{pt}</li>
+                                  ))}
+                                </ul>
+                              }
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  ))
+                  }
+                </div>
+              </div>
+              <div className="card border-primary mb-3">
+                <div className="card-header">
+                  <p style={{ fontSize: '20px' }} className="ms-2 mb-n1">Achievements</p>
+                </div>
+                <div className="card-body">
+                  {this.data.achievements.map(achievement => (
+                    <div className="mb-3">
+                      <div className="hstack card-title">
+                        <h5 className="mt-n1 mb-n2 ms-2 ">{achievement.title + " "}</h5>
+                        <label className="mt-n1 mb-n2 ms-2 ">({achievement.year})</label>
+                      </div>
+                      <div className="mt-2 ms-2 ">
+
+                        <ul className="mb-0">
+                          {achievement.description.map(desc => (
+                            (typeof desc === 'string') ?
+                              <li>
+                                {desc}
+                              </li>
+                              :
+                              <li>
+                                {desc.desc}
+                                <ul className="mb-0">
+                                  {desc.pt.map(pt => (
+                                    <li>{pt}</li>
+                                  ))}
+                                </ul>
+                              </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="card border-primary mb-3">
+                <div className="card-header">
+                  <p style={{ fontSize: '20px' }} className="ms-2 mb-n1">Areas Of Expertise</p>
+                </div>
+                <div className="card-body">
+                  <div>
+                    <h5>Experience In</h5>
+                    <div className="row ms-n2 me-2">
+                      <img src="/images/logo_java.png" className="ms-n2 mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                      <img src="/images/logo_python.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                      <img src="/images/logo_mysql.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                    </div>
+                  </div>
+                  <div className=" mt-4">
+                    <h5>Fundamental In Web Programming</h5>
+                    <div className="row ms-0 me-1">
+                      <img src="/images/logo_web.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                    </div>
+                  </div>
+                  <div className=" mt-4">
+                    <h5>Fundamental In Mobile Application Development</h5>
+                    <div className="row ms-1 me-1">
+                      <img src="/images/logo_android_studio.png" className=" mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                    </div>
+                  </div>
+                  <div className=" mt-4 mb-3">
+                    <h5>Little Experience In Web Frameworks</h5>
+                    <div className="row ms-n2 ">
+                      <img src="/images/logo_react.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                      <img src="/images/logo_django.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                      <img src="/images/logo_spring_boot.png" className="mx-auto col"
+                        style={{ width: '50px', height: 'auto' }} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card border-primary mb-2">
+                <div className="card-header">
+                  <p style={{ fontSize: '20px' }} className="ms-2 mb-n1">Languages</p>
+                </div>
+                <div className="card-body">
+                  {this.data.languages.native.length > 0 ?
+                    <div className="mb-3">
+                      <h5>Beginner in</h5>
+                      <div className=" text-center">
+                        {this.data.languages.native.map(language => (
+                          <p>{language}</p>
+                        ))}
+                      </div>
+                    </div>
+                    : ""
+                  }
+                  {this.data.languages.fluent.length > 0 ?
+                    <div className="mb-3">
+                      <h5>Fluent in</h5>
+                      <div className=" text-center">
+                        {this.data.languages.fluent.map(language => (
+                          <p className="mb-0">{language}</p>
+                        ))}
+                      </div>
+                    </div>
+                    : ""
+                  }
+                  {this.data.languages.proficient.length > 0 ?
+                    <div className="mb-3">
+                      <h5>Proficient in</h5>
+                      <div className=" text-center">
+                        {this.data.languages.proficient.map(language => (
+                          <p>{language}</p>
+                        ))}
+                      </div>
+                    </div>
+                    : ""
+                  }
+                  {this.data.languages.intermediate.length > 0 ?
+                    <div className="mb-3">
+                      <h5>Intermediate in</h5>
+                      <div className=" text-center">
+                        {this.data.languages.intermediate.map(language => (
+                          <p>{language}</p>
+                        ))}
+                      </div>
+                    </div>
+                    : ""
+                  }
+                  {this.data.languages.beginner.length > 0 ?
+                    <div className="mb-3">
+                      <h5>Beginner in</h5>
+                      <div className="text-center">
+                        {this.data.languages.beginner.map(language => (
+                          <p>{language}</p>
+                        ))}
+                      </div>
+                    </div>
+                    : ""
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/contents/resume.js
@@ -0,0 +1,84 @@
+export function resume() {
+    var data = {
+        profile_img: "img_profile.jpg",
+        name: "Fan Siew Ying",
+        subtitle: "Software Engineeringing Graduate With Honours",
+        location: "Bentong, Pahang",
+        phone: "012-9513528",
+        email: "fsying1999@gmail.com",
+        linkedin: "linkedin.com/in/fsying99",
+        educations: [
+            {
+                img: "logo_um.png",
+                certificate: "BACHELOR OF COMPUTER SCIENCE (SOFTWARE ENGINEERING)",
+                location: "University of Malaya, WP Kuala Lumpur",
+                graduation_date: "Feb 2022",
+                result: "CGPA 3.66/4.0",
+                key_modules: "Fundamentals of Programming, Database, Software Testing, Web programming, Mobile Application Development"
+            }
+        ],
+        works: [
+            {
+                img: "logo_si.jpg",
+                position: "INTERN SOFTWARE ENGINEER",
+                location: "Software International Corporation (M) Sdn Bhd, WP Kuala Lumpur",
+                responsibilities: [
+                    "User Acceptance testing (UAT) on existing application",
+                    "Bugs fixing on developing application (ReactJs & Django framework)",
+                    "Development in new application's Proof Of Concept(POC) (Spring framework)"
+                ]
+            }
+        ],
+        achievements: [
+            {
+                title: "School Project",
+                year: "2018-2022",
+                description: [
+                    {
+                        desc: "Honed project management skills and was the project leader for:",
+                        pt:
+                            [
+                                "MazeRunner - a maze runner game to learn the fundamental of Java programming",
+                                "VillagersStory - a Java project on a virtual environment which applied design patterns",
+                                "TimeTask - a mobile application developed using Android Studio with Java which allows user to manage timetable and tasks"
+                            ]
+                    }
+                ]
+            },
+            {
+                title: "Final Year Project",
+                year: "2021-2022",
+                description: [
+                    "Development on Learning Management System for Young Students",
+                    "Application of ReactJs and Django framework for project development",
+                    "Application of component-based architecture, software development life cycle, and project management"
+                ]
+            },
+            {
+                title: "UM Wushu Club",
+                year: "2018-2021",
+                description: [
+                    "Won 2 Gold and 2 Bronze in 2018 Wucui Wushu Intervarsity Championship",
+                    "Experienced Wushu athlete and team leader",
+                    "Conducted online classes for Wushu practice for around 20 members"
+                ]
+            },
+            {
+                title: "Sukan Mahasiswa UM (SUKMUM)",
+                year: "2018 & 2019",
+                description: [
+                    "Silver medalist for Womens Long Jump and Triple Jump in SUKMUM Track and Field events",
+                    "Represented the residental college volleyball team in Volleyball events"
+                ]
+            }
+        ],
+        languages: {
+            beginner: ["Japanese"],
+            intermediate: [],
+            proficient: ["English", "Bahasa Malaysia"],
+            fluent: ["Mandarin"],
+            native: [],
+        }
+    };
+    return data;
+}
